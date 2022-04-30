3837d15317cb211e6be957e17c84fa8821b27369
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-04-30 12:27:29 +0200

    favicon and insert comics in view

--- resources/views/pages/comics.blade.php
+++ resources/views/pages/comics.blade.php
@@ -4,9 +4,37 @@
 
 @section('mainContent')
 
-<!-- jumbo -->
-<section class="jumbo">
-    <img src="{{ asset('assets/img/jumbotron.jpg') }}" >
+<section class="main">
+
+    <!-- jumbo -->
+    <section class="jumbo">
+        <img src="{{ asset('assets/img/jumbotron.jpg') }}" >
+    </section>
+
+    <div class="container">
+        <!-- title -->
+        <button class="btn-series">Current series</button>
+
+        <!-- thumbs wrapper -->
+        <div class="series-wrapper">
+            
+            @foreach ($comics as $comic)
+            <div class="dc__thumb">
+
+                <figure class="thumb-img">
+                    <img src="{{ $comic['thumb'] }}">
+                </figure>
+                
+                <h4 class="thumb-title">{{ $comic['series']}}</h4>
+        
+            </div>                
+            @endforeach
+
+        </div>
+    </div>
+    
+    <!-- button load -->
+    <button class="btn-load">Load More</button>
 </section>
 
 @include('componentes.mainCategories')
