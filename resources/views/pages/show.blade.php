1e70c4a39ca90958d0f74d28cd55096aed0b0bf3
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-05-02 10:23:33 +0200

    COMPLETE

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -29,9 +29,9 @@
 
 
 {{-- title and descriptioncomic book --}}
-<section class="container info-wrapper">
+<section class="container book-wrapper">
 
-    <div class="info-book">
+    <div class="book-card">
         {{-- title --}}
         <h1>{{ $comic['title'] }}</h1>
 
@@ -61,18 +61,92 @@
 
 
 {{-- talent & spec book --}}
-<section class="talent-specs">
+<section class="comic-info">
     <div class="container">
 
-        <div class="talent-book">
-            <h2 class="">Talent</h2>
+        <div class="col-2">
+            <h2 class="col-title">Talent</h2>
+
+            <div class="info-wrapper">
+                <div class="info-title">
+                    <p>Art by:</p>
+                </div>
+
+                <div class="info-detail">
+                    <p>                        
+                        @foreach ($comic['artists'] as $key => $artist)
+                        
+                        <span class="text-blue">
+                            <a href="#">
+                                {{ $artist }}</span>@if($key < count($comic['artists'])-1),@endif
+                            </a>
+        
+                        @endforeach                        
+                    </p>
+                </div>
+            </div>
+
+            <div class="info-wrapper">
+                <div class="info-title">
+                    <p>Written by:</p>
+                </div>
+
+                <div class="info-detail">
+                    <p>                        
+                        @foreach ($comic['writers'] as $key => $writer)
+                        
+                        <span class="text-blue">
+                            <a href="#">
+                                {{ $writer }}</span>@if($key < count($comic['writers'])-1),@endif
+                            </a>
+        
+                        @endforeach                        
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-2">
+            <h2 class="col-title">Specs</h2>
+
+            <div class="info-wrapper">
+                <div class="info-title">
+                    <p>Series:</p>
+                </div>
+
+                <div class="info-detail">
+                    <p class="text-blue text-type">                        
+                        <a href="#">{{$comic['type']}}  </a>                      
+                    </p>
+                </div>
+            </div>
+
+            <div class="info-wrapper">
+                <div class="info-title">
+                    <p>U.S. Price:</p>
+                </div>
+
+                <div class="info-detail">
+                    <p>                        
+                        {{$comic['price']}}                        
+                    </p>
+                </div>
+            </div>
+
+            <div class="info-wrapper">
+                <div class="info-title">
+                    <p>On Sale Date:</p>
+                </div>
+
+                <div class="info-detail">
+                    <p>                        
+                        {{ date('M d Y', strtotime($comic['sale_date'])) }}                        
+                    </p>
+                </div>
+            </div>
 
         </div>
 
-        <div class="specs-book">
-            <h2 class="">Spacs</h2>
-
-        </div>
     </div>
 </section>
 
